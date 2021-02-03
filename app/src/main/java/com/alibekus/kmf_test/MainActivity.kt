2e4b7aa884eeaf9e4c14f06a7196ebffcbc6e486
alibek.nurgaliyev <alibek.nurgaliyev@gmail.com>
2021-02-03 19:21:56 +0600

    added buttons and activities

--- app/src/main/java/com/alibekus/kmf_test/MainActivity.kt
+++ app/src/main/java/com/alibekus/kmf_test/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.alibekus.kmf_test
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private val registrationButton: Button by lazy {
+        findViewById(R.id.activity_main_button_registration)
+    }
+
+    private val userInfoButton: Button by lazy {
+        findViewById(R.id.activity_main_button_user_info)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        registrationButton.setOnClickListener {
+            navigateToRegistrationActivity()
+        }
+
+        userInfoButton.setOnClickListener {
+            navigateToUserInfoActivity()
+        }
+    }
+
+    private fun navigateToRegistrationActivity() {
+        val registrationIntent = Intent(this, RegistrationActivity::class.java)
+        startActivity(registrationIntent)
+    }
+
+    private fun navigateToUserInfoActivity() {
+        val userInfoIntent = Intent(this, UserInfoActivity::class.java)
+        startActivity(userInfoIntent)
     }
 }
